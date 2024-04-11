bd51e0d63d36f3d1db3f0e48df30f56f1cf62729
Juliaps26 <124824062+Juliaps26@users.noreply.github.com>
2024-04-11 06:30:13 -0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// document.getElementById('background').style.backgroundImage = "url('../img/barbiecapa.png')";
+
+
+const imagePath = '../img/barbiecapa.png';
+
+        // Selecionar o elemento de fundo
+        const backgroundElement = document.getElementById('background');
+
+            // Calcular a altura desejada (por exemplo, metade da largura da tela)
+            const windowHeight = window.innerHeight;
+            const windowWidth = window.innerWidth;
+            const desiredHeight = windowWidth / 2.5; // Ajuste conforme necessário
+
+        // Definir estilos CSS para o elemento de fundo
+        backgroundElement.style.backgroundImage = `url('${imagePath}')`;
+        backgroundElement.style.backgroundRepeat = 'no-repeat';
+        backgroundElement.style.backgroundSize = 'cover';
+        backgroundElement.style.backgroundPosition = 'center';
+        backgroundElement.style.height = `${desiredHeight}px`;
